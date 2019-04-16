86286560928d185b360c1351abac1c47605aa825
pharzan <farzan.tinati@gmail.com>
2019-04-17 00:35:37 +0300

    added comments page

--- components/Comments.js
+++ components/Comments.js
@@ -5,18 +5,68 @@ import moment from "moment";
 export class Comments extends React.Component {
   constructor(props) {
     super(props);
-    (this.state = { comments: {} }), (this.loaded = false);
+    this.state = { comments: {}, post: {} };
+    this.loaded = false;
   }
-  componentDidMount(){
-  } 
 
-  render() {
+  getCurrent(postId) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://www.reddit.com/api/info.json?id=${postId}`)
+        .then(res => res.json())
+        .then(result => {
+          return resolve(result.data);
+        });
+    });
+  }
+
+  parseComments(comments, i, j) {
+    for (let property in comments) {
+      if (comments.hasOwnProperty(property)) {
+        if (property === "replies") {
+          this.parseComments(comments[property], i, j);
+        } else if (typeof comments[property] === "object") {
+          if (comments[property] != null) {
+            if (comments[property].body != undefined) {
+              console.log(".".repeat(j), property, comments[property].body);
+              j += 1;
+            }
+            this.parseComments(comments[property], i, j);
+          }
+        }
+      } else {
+        console.log("!!!HERE");
+      }
+    }
+  }
+  
+  getComments() {
+    // const url = `https://www.reddit.com/r/${subreddit}/comments/${postId}.json`;
+
+    const url = `https://www.reddit.com/r/redditdev/comments/bdgd0o.json`;
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(result => {
+          return resolve(result);
+        });
+    });
+  }
+
+  componentDidMount() {
     const { navigation } = this.props;
     const post = navigation.getParam("post", "post");
+    this.setState({
+      post: post.item
+    });
+    this.getComments().then(result => {
+      this.parseComments(result, 0, 0);
+    });
+  }
 
+  render() {
     return (
       <View>
-        <Text>{post.item.title}</Text>
+        <Text>{this.state.post.title}</Text>
       </View>
     );
   }
