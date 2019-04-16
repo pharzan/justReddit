e8e95af9cd04d0ca1bcb67ce220e7c1ee94d3bd2
pharzan <farzan.tinati@gmail.com>
2019-04-16 12:55:16 +0300

    made home as a component

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
-import React from 'react';
-import {
-  StyleSheet
-} from "react-native";
-
-import { Icon } from "react-native-elements";
-
-import { List, ListItem } from "react-native-elements";
-//react-navigation
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import moment from "moment";
 
 import {Settings} from './components/Settings.js';
 
 import {Home} from './components/Home.js';
-
-console.log("^^^",Settings)
-
-
-
 
 const MainNavigator = createStackNavigator({
   Home: { screen: Home },
@@ -26,11 +12,3 @@ const App = createAppContainer(MainNavigator);
 export default App;
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,14 @@
-import {
-    Text,
-    View,
-} from "react-native";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
 export class Home extends React.Component {
   render() {
